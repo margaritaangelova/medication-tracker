1de3d34cac3ff571f00645c9e0887cdb1919ac9d
margaritaangelova <74078034+margaritaangelova@users.noreply.github.com>
2021-04-29 15:19:42 +0300

    Revert "Creating project services"

--- medTracker/src/app/app.module.ts
+++ medTracker/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import {HttpClientModule} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -11,16 +10,8 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [
-    BrowserModule, 
-    IonicModule.forRoot(), 
-    AppRoutingModule, 
-    HttpClientModule
-  ],
-  providers: [{ 
-    provide: RouteReuseStrategy, 
-    useClass: IonicRouteStrategy 
-  }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
